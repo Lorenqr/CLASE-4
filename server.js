61b6f98c981c4cfa1e510e1fcdc2047897eb759c
lorenqr <lorenagr223@gmail.com>
2024-10-03 21:14:56 -0500

    Creacion de repositorio

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+const puerto = 3000
+
+let usuarios = [{
+    "id":1,
+    "nombre": "Lorena Ospina",
+    "edad": 25,
+}]
+
+app.get('/usuarios', (req,res) => {
+    res.json(usuarios)
+})
+
+app.post('/usuarios', (req, res) => {
+    console.log(req.body) //muestra en la consola el cuerpo de la solicitud
+
+    const newUser = { ...req.body, id: usuarios.length +1 };
+    usuarios.push(newUser);
+    res.send('Se ha creado un usuario');
+});
+
+
+
+app.listen(puerto, () => {
+    console.log(`Servidor esperando solicitud ${puerto}`)
+})
+
